cee90c4078362dab4978fa821eff2b0a5244fc09
Alexander Bokov <alexander.bokov30@yandex.ru>
2024-07-13 18:01:02 +0300

    start project

---
+++ src/test/java/ru/netology/banklogin/page/VerificationCodePage.java
@@ -0,0 +1,33 @@
+package ru.netology.banklogin.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class VerificationCodePage {
+    private SelenideElement codeField = $("[data-test-id='code'] input");
+    private SelenideElement verifyButton = $("[data-test-id='action-verify']");
+    private SelenideElement invalidNotificationText = $("[data-test-id='error-notification'] .notification__content");
+
+    public VerificationCodePage() {
+        verifyButton.shouldBe(Condition.visible);
+    }
+
+    private void makeValidCode(String code) {
+        codeField.setValue(code);
+        verifyButton.click();
+    }
+
+    public DashboardPage validCodeEnter(String code) {
+        makeValidCode(code);
+
+        return new DashboardPage();
+    }
+
+    public void invalidCodeEnter(String code) {
+        makeValidCode(code);
+
+        invalidNotificationText.shouldHave(Condition.text("Ошибка! Неверно указан код! Попробуйте ещё раз.")).shouldBe(Condition.visible);
+    }
+}
